bbd9ec11ad2fa7d016c76fe04c06823b286152b2
Tomo <tc.maxam@gmail.com>
2019-09-10 00:19:57 +0900

    Add to set config

--- cluster.sh
+++ cluster.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 set -eu
 
+PROJECT=kube-chiku
 CLUSTER=k8s
 ZONE=asia-northeast1-a
+
+# set config
+gcloud config set project $PROJECT
 
 # create cluster
 gcloud container clusters create $CLUSTER --zone $ZONE --num-nodes 1
