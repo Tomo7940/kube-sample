8bc88db7695e54f9ab922f9a7e45887820e9ad39
Tomo <tc.maxam@gmail.com>
2019-09-09 23:13:16 +0900

    Add echo

--- build.sh
+++ build.sh
@@ -13,12 +13,16 @@ DOCKER_PROJECT=kube-chiku
 DOCKER_IMAGE=go-app
 
 # Build docker image
+echo "building .."
 docker build -t $DOCKER_IMAGE:$VERSION .
+echo "tagging .."
 docker tag $DOCKER_IMAGE:$VERSION $DOCKER_HOST/$DOCKER_PROJECT/$DOCKER_IMAGE:$VERSION
 
 # Push the image to Private Docker Registry
+echo "pushing .."
 docker push $DOCKER_HOST/$DOCKER_PROJECT/$DOCKER_IMAGE:$VERSION
 
 # Clean the image
+echo "removing .."
 docker rm $(docker ps -aq)
 docker images | awk '/<none/{print $3}' | xargs docker rmi 
